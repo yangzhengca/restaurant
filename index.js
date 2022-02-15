43646f867d0a78a0c36a025ab51bbc3cc3b6eff6
ba-ahmedsatti <aihsatti@gmail.com>
2022-02-15 12:25:04 -0700

    Restaurant Project

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.set('view engine', 'pug');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index', {});
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
