a0f18202056c305f578f7ca95dd72ee8ea5caa0d
ba-ahmedsatti <aihsatti@gmail.com>
2022-02-27 17:59:27 -0700

    fixed server issue

--- menu.js
+++ menu.js
@@ -8,17 +8,17 @@ app.use(express.static('public'));
 
 app.get("/", (req, res) => {
   const menus = [
-    { name: "burger", image: "menu.jpg" },
-    { name: "salad", image: "menu1.jpg" },
-    { name: "shrimp", image: "menu3.jpg" },
-    { name: "pasta", image: "menu4.jpg" },
-    { name: "steak", image: "menu6.jpg" },
-    { name: "pizza", image: "menu7.jpg" },
-    { name: "fish", image: "menu8.jpg" },
-    { name: "ribeye", image: "menu9.jpg" },
+    { name: 'burger', image: 'menu.jpg' },
+    { name: 'salad', image: 'menu1.jpg' },
+    { name: 'shrimp', image: 'menu3.jpg' },
+    { name: 'pasta', image: 'menu4.jpg' },
+    { name: 'steak', image: 'menu6.jpg' },
+    { name: 'pizza', image: 'menu7.jpg' },
+    { name: 'fish', image: 'menu8.jpg' },
+    { name: 'ribeye', image: 'menu9.jpg' },
   ];
 
-  res.render("menu", { menus });
+  res.render('menu', { menus });
 });
 
 app.listen(port, () => {
